d70ae633b3450fe738fe4abc4ebe4e9eeab9f39a
Bartosz Azymi <bartosz.azymi@gmail.com>
2024-06-11 16:40:03 +0200

    SearchIcon

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,8 +5,32 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Center(
-      child: Text('start scren'),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            'learn flutter',
+            style: TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+              fontSize: 23,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text('Start quiz'))
+        ],
+      ),
     );
   }
 }
