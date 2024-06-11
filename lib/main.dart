d737bcb8ea4c1a6da80cbe83d2a7a79eb4288f7b
Bartosz Azymi <bartosz.azymi@gmail.com>
2024-06-11 16:14:50 +0200

    Bg&StartScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_button.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.purple,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Image(
-              image: AssetImage('assets/quiz-logo.png'),
-              width: 100,
-              height: 100,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Hello',
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            Startbutton(
-              onPressed: () {
-                print('Button pressed');
-              },
-            ),
-          ],
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 82, 18, 194),
+              Color.fromARGB(255, 151, 98, 241),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const StartScreen(),
         ),
       ),
     ),
-  ));
+  );
 }
